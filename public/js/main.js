e14c8cfeab2e7c1dc0c8e9d855b4998b9ec7516a
Angela <antunezangela89@gmail.com>
2023-12-07 12:01:35 +0100

    Finish local storage

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+// Guardar el nombre del player
+//Ir a la página de game con el botón
+
 document.addEventListener("DOMContentLoaded", main);
 
 function main () {
@@ -6,6 +9,7 @@ function main () {
 
 function submitForm (event) { //handler del evento cuando se envía el formulario
     event.preventDefault();
+    //let playerName = event.target.player.value; ya no hace falta est línea
     localStorage.setItem("playerName", event.target.player.value);
     location.pathname ="/public/pages/game.html";
 }
