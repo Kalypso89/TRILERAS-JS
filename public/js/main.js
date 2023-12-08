2714d854ff862d78bbfe2cea585937cbdc55fa06
Angela <antunezangela89@gmail.com>
2023-12-08 02:00:44 +0100

    Finish submitForm function

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,8 @@ function main () {
     document.getElementById("start__form").addEventListener("submit",submitForm);
 };
 
+function submitForm (event) { //handler del evento cuando se envía el formulario
+    event.preventDefault();
+    localStorage.setItem("playerName", event.target.player.value);
+    location.pathname ="/public/pages/game.html";
+}
